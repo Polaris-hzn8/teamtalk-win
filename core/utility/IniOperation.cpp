d846ce50e4bcad44645054c4523252301afa3062
Polaris-hzn8 <3453851623@qq.com>
2025-02-15 09:28:24 +0800

    调整

--- core/utility/IniOperation.cpp
+++ core/utility/IniOperation.cpp
@@ -1,8 +1,15 @@
-/******************************************************************************* 
- *  @file      IniOperation.cpp 2014\12\30 15:27:52 $
- *  @author    ´ó·ð<dafo@mogujie.com>
- *  @brief     Source: http://www.codeproject.com/Articles/10809/A-Small-Class-to-Read-INI-File ////
- ******************************************************************************/
+
+/**
+* Copyright (C) 2024 Polaris-hzn8 / LuoChenhao
+*
+* Author: luochenhao
+* Email: lch2022fox@163.com
+* Time: Tue 08 Oct 2024 00:28:16 CST
+* Github: https://github.com/Polaris-hzn8
+* Src code may be copied only under the term's of the Apache License
+* Please visit the http://www.apache.org/licenses/ Page for more detail.
+*
+**/
 
 #include "stdafx.h"
 #include "utility/IniOperation.h"
--- include/utility/Multilingual.h
+++ include/utility/Multilingual.h
@@ -1,42 +1,34 @@
-/*******************************************************************************
- *  @file      Multilingual.h 2014\11\25 21:14:44 $
- *  @author    快刀<kuaidao@mogujie.com>
- *  @brief     多语言支持工具类，利用windows ini api实现
- ******************************************************************************/
+
+/**
+* Copyright (C) 2024 Polaris-hzn8 / LuoChenhao
+*
+* Author: luochenhao
+* Email: lch2022fox@163.com
+* Time: Tue 08 Oct 2024 00:28:16 CST
+* Github: https://github.com/Polaris-hzn8
+* Src code may be copied only under the term's of the Apache License
+* Please visit the http://www.apache.org/licenses/ Page for more detail.
+* brief: 多语言支持工具类 利用windows IniApi实现
+**/
 
 #ifndef MULTILINGUAL_93AF83ED_9E20_45F1_8C1A_48CA7C6D3FF9_H__
 #define MULTILINGUAL_93AF83ED_9E20_45F1_8C1A_48CA7C6D3FF9_H__
 
 #include "GlobalDefine.h"
 #include "utility/utilityDll.h"
-/******************************************************************************/
+
 NAMESPACE_BEGIN(util)
 
-/**
- * The class <code>Multilingual</code> 
- *
- */
 class UTILITY_CLASS Multilingual
 {
 public:
-    /** @name Constructors and Destructor*/
-
-    //@{
-    /**
-     * Constructor 
-     */
     Multilingual();
-    /**
-     * Destructor
-     */
     ~Multilingual();
-    //@}
-
 public:
 	CString getStringById(LPCTSTR strID);
 };
 extern UTILITY_API Multilingual* getMultilingual();
 
 NAMESPACE_END(util)
-/******************************************************************************/
+
 #endif// MULTILINGUAL_93AF83ED_9E20_45F1_8C1A_48CA7C6D3FF9_H__
