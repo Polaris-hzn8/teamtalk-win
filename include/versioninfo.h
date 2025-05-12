3c0a5e2e1fcb5d93d555da8dee41df73b30b8b92
luochenhao <luochenhao@huatusoft.com>
2025-05-12 15:31:53 +0800

    更新

--- include/versioninfo.h
+++ include/versioninfo.h
@@ -1,13 +1,18 @@
-/*******************************************************************************
- *  @file      versioninfo.h 2015\3\23 20:01:28 $
- *  @author    ¿ìµ¶<kuaidao@mogujie.com>
- *  @brief     
- ******************************************************************************/
+/**
+* Copyright (C) 2024 Polaris-hzn8 / LuoChenhao
+*
+* Author: luochenhao
+* Email: lch2022fox@163.com
+* Time: Fri 08 Nov 2024 11:52:01 CST
+* Github: https://github.com/Polaris-hzn8
+* Src code may be copied only under the term's of the Apache License
+* Please visit the http://www.apache.org/licenses/ Page for more detail.
+*
+**/
 
 #ifndef VERSIONINFO_0AA68762_4EB8_405A_BC84_BDE26633E924_H__
 #define VERSIONINFO_0AA68762_4EB8_405A_BC84_BDE26633E924_H__
 
 #define TEAMTALK_VERSION        1
 
-/******************************************************************************/
 #endif// VERSIONINFO_0AA68762_4EB8_405A_BC84_BDE26633E924_H__
